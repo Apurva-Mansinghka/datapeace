b4c68fb970838eaed2f9a2d99c2e4097760a8c3f
Apurva <apurva.mansinghka@gmail.com>
2022-01-14 23:58:49 +0530

    Update urls.py

--- datapeace/urls.py
+++ datapeace/urls.py
@@ -1,19 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework import routers
 
 import datapeaceApp.views
 import datapeaceApp.api_views
 
+router = routers.DefaultRouter()
+router.register(r'users', datapeaceApp.views.UserViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('api/users/', datapeaceApp.api_views.UserList.as_view()),
     path('api/users/new', datapeaceApp.api_views.UserCreate.as_view()),
     path('api/users/<int:id>/update', datapeaceApp.api_views.UserUpdate.as_view()),
-
-    path('admin/', admin.site.urls),
-    path('api/users/<int:id>/', datapeaceApp.views.show, name='show-user'),
- #   path('', store.views.index, name='list-products'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/users/<int:id>/delete', datapeaceApp.api_views.UserDestroy.as_view()),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    ] 
